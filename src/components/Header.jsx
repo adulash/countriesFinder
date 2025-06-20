4909aae4da592f79e50b10803e6502d88ccb8c97
adulash <a.walidii@gmail.com>
2025-06-21 00:08:18 +0300

    Align header with content + center layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import ThemeSwitcher from "./ThemeSwitcher";
 function Header() {
   return (
     <header className="mb-6 bg-white shadow md:mb-12 dark:bg-gray-800">
-      <div className="container mx-auto px-5 md:px-0">
+      <div className="mx-auto w-full max-w-6xl px-5 md:px-0">
         <div className="flex h-20 items-center justify-between">
           <Logo />
           <ThemeSwitcher />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,8 +12,8 @@ function Home() {
       {isError && <ShowMessage message="Something went wrong !!" />}
       {isLoading && <ShowMessage message="Loading countries... " />}
       {!isError && !isLoading && (
-        <div className="container mx-auto px-5 md:px-0">
-          <div className="flex flex-col justify-between gap-10 md:h-14 md:flex-row md:gap-0">
+        <div className="flex flex-col justify-center items-center min-h-[calc(100vh-80px)] px-5 md:px-0">
+          <div className="w-full max-w-6xl flex flex-col justify-between gap-10 md:h-14 md:flex-row md:gap-0 mb-10">
             <SearchInput contriesList={setResult} filteredCountries={setfilteredList} />
             <RegionMenu contriesList={setResult} filteredCountries={setfilteredList} />
           </div>
